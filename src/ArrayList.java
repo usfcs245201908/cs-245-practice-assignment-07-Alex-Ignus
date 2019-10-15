a2e3c519320f3e724014c4d997586c6589b95e00
Alexander Wojcik <44510036+Alex-Ignus@users.noreply.github.com>
2019-10-15 16:06:23 -0700

    first commit

---
+++ src/ArrayList.java
@@ -0,0 +1,27 @@
+public class ArrayList<T> implements List<T> {
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(int pos, T item) {
+
+    }
+
+    @Override
+    public T get(int pos) {
+        return null;
+    }
+
+    @Override
+    public T remove(int pos) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/LinkedList.java
@@ -0,0 +1,26 @@
+public class LinkedList implements List {
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(int pos, Object item) {
+
+    }
+
+    @Override
+    public Object get(int pos) {
+        return null;
+    }
+
+    @Override
+    public Object remove(int pos) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
